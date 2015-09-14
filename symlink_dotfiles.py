c0a0f73d128c9843e37950c45475b84758c5aad5
Colin Dunklau <colin.dunklau@gmail.com>
2015-09-14 07:20:31 -0500

    Use symlinks instead of home-as-repo

---
+++ symlink_dotfiles.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import sys
+import os
+import shutil
+
+
+this_dir = os.path.dirname(os.path.abspath(__file__))
+home_dir = os.path.expanduser('~')
+
+dotfiles = ('.bashrc', '.vimrc', '.pythonrc', '.tmux.conf')
+
+backup_suffix = '.bck'
+
+
+def main():
+    for dotfile in dotfiles:
+        homedot = os.path.join(home_dir, dotfile)
+        if nonlink(homedot):
+            backup = homedot + backup_suffix
+            print_stderr(
+                'Backing up regular file {0} to {1}'.format(homedot, backup))
+            shutil.copy(homedot, backup)
+        target = os.path.join(this_dir, dotfile)
+        link_name = os.path.join(home_dir, dotfile)
+        print_stderr('Removing {0}'.format(link_name))
+        os.remove(link_name)
+        print_stderr(
+            'Creating symbolic link {0} -> {1}'.format(link_name, target))
+        os.symlink(target, link_name)
+
+
+def nonlink(dotfile):
+    return os.path.isfile(dotfile) and not os.path.islink(dotfile)
+
+
+def print_stderr(*args):
+    print(*args, file=sys.stderr)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
