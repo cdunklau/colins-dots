1ab773e01f48bcf027e19b12be482757b7f29392
Colin Dunklau <colin.dunklau@gmail.com>
2016-04-01 09:31:53 -0500

    Fix #1 fails on nonexistant files

--- symlink_dotfiles.py
+++ symlink_dotfiles.py
@@ -2,6 +2,7 @@
 from __future__ import print_function
 import sys
 import os
+import errno
 import shutil
 
 
@@ -24,7 +25,11 @@ def main():
         target = os.path.join(this_dir, dotfile)
         link_name = os.path.join(home_dir, dotfile)
         print_stderr('Removing {0}'.format(link_name))
-        os.remove(link_name)
+        try:
+            os.remove(link_name)
+        except OSError as e:
+            if e.errno != errno.ENOENT:
+                raise
         print_stderr(
             'Creating symbolic link {0} -> {1}'.format(link_name, target))
         os.symlink(target, link_name)
